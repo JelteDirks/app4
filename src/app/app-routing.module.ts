ec61fd6efecaf980ffdb835a80c918dd8fcb686f
Jelte Dirks <j.dirks@outlook.com>
2019-05-25 20:43:09 +0200

    add auth path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
   { path: 'contact', loadChildren: './contact/contact.module#ContactPageModule' },
+  { path: 'auth', loadChildren: './auth/auth.module#AuthPageModule' },
 ];
 
 @NgModule({
