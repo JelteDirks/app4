5284c28c6346f0e5fa1be02e18ec501c5eaee634
Jelte Dirks <j.dirks@outlook.com>
2019-05-25 20:59:46 +0200

    add login form template and hook for submit

--- src/app/auth/auth.page.ts
+++ src/app/auth/auth.page.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {NgForm} from "@angular/forms";
 
 @Component({
-  selector: 'app-auth',
-  templateUrl: './auth.page.html',
-  styleUrls: ['./auth.page.scss'],
+    selector: 'app-auth',
+    templateUrl: './auth.page.html',
+    styleUrls: ['./auth.page.scss'],
 })
 export class AuthPage implements OnInit {
 
-  constructor() { }
+    constructor() {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
+    public onSubmit(loginForm: NgForm) {
+    }
 }
